1ee75a6cce3959dc6e0adc45b65831b4c6785ff5
TestLeafPages <earth@testleaf.com>
2023-01-22 18:00:29 +0530

    Update AmazonList.java

--- week4/day1/AmazonList.java
+++ week4/day1/AmazonList.java
@@ -28,16 +28,17 @@ step5:Print the price in order
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://www.amazon.in/");
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Mobile",Keys.ENTER);
+		
+List<String> lst=new ArrayList<String>();//Store String of data into list
 		List<WebElement> price = driver.findElements(By.xpath("//span[@class='a-price-whole']"));
 		for (int i = 0; i < price.size(); i++) {
-			String num = price.get(i).getText();
-		List<String> lst=new ArrayList<String>();//Store String of data into list
-		for (int j = 0; j < num.length(); j++) {
-			lst.add(num);
+		String num = price.get(i).getText();
+		lst.add(num);
+		System.out.println(num);
 		}
 		Collections.sort(lst);
-   System.out.println(lst);
-	}
+		System.out.println(lst);
+		
 	}
 }
+
